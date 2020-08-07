fba6c0215bb8975ad477eada26d504a2fa2fabd3
kikko2796 <kikko2796@gmail.com>
2020-08-07 19:07:44 +0900

    単数検索のみ出来上がり

--- app/models/day.rb
+++ app/models/day.rb
@@ -2,9 +2,11 @@ class Day < ApplicationRecord
    #searchメソッドの定義
    def self.search(search)
     if search
-        where(['name LIKE ?', "%#{search}%"])
+        Day.where("name LIKE ?","%#{search}%")
+        #Day.where(name: "%#{search}%").where(date: like = ?, "%#{search}%")
+        #Day.where("name = ? AND date LIKE ?" , "%#{search}%" , "%#{search}%")
     else
-        all
+        Day.all
     end
    end
 end
